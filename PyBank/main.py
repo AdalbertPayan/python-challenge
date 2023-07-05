000cfcb1e733598ae5079b9429dc25756721b6e6
Adalbert Payan <adalbertpayan@APiMac.local>
2023-07-04 22:11:14 -0700

    update, need Simon, tutor to help with Average change, it's wrongright now

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,43 +16,54 @@ with open(pybank_csv) as csvfile:
     profit_loss_sum = 0
     greatest_profit_increase = 0
     greatest_profit_decrease = 999999999999999999999
+    profit_loss_average = 0
+    greatest_profit_increase_date = 0
+    greatest_profit_decrease_date = 0
+    final_profit_loss = 0
 
 
     #loop through rows in csv file
     for row in csvreader:
         #calculate total number of months included in dataset
-        months_total = months_total + 1
+        months_total += 1
 
 
         #create variable for total profit loss and calculate total profit loss
-        profit_loss = row[1]
-        profit_loss_total = profit_loss_total + int(profit_loss)
+        profit_loss = int(row[1])
+        profit_loss_total += profit_loss
 
         #calculate The changes in "Profit/Losses" over the entire period, and (then the average of those changes)
         if months_total > 1:
-            change = int(profit_loss) - final_profit_loss
+            change = profit_loss - final_profit_loss
 
-            profit_loss_sum = profit_loss_total + change
-            profit_loss_average = profit_loss_total / (months_total - 1)
+            profit_loss_sum += change
 
         #calculate greatest increase in profits (date and amount) over the entire period
-        if change > greatest_profit_increase:
-            greatest_profit_increase = change
-            greatest_profit_increase_date = row[0]
+            if change > greatest_profit_increase:
+                greatest_profit_increase = change
+                greatest_profit_increase_date = row[0]
 
          #calculate greatest decrease in losses (date and amount) over the entire period
-        if change < greatest_profit_decrease:
-            greatest_profit_decrease = change
-            greatest_profit_decrease_date = row[0]
+            if change < greatest_profit_decrease:
+                greatest_profit_decrease = change
+                greatest_profit_decrease_date = row[0]
 
         #create financial analysis and print total months, total profit loss, average change, greatest increase in profits, greatest decrease in profits
-            final_profit_loss = (row[1])
+        final_profit_loss = profit_loss
         months = (f"Total Months: {months_total}")
         total = (f"Total: ${profit_loss_total}")
         average = (f"Average Change: ${profit_loss_average}")
         increase = (f"Greatest Increase in Profits: {greatest_profit_increase_date} (${greatest_profit_increase})")
         decrease = (f"Greatest Decrease in Profits: {greatest_profit_decrease_date} (${greatest_profit_decrease})")
 
+        #calculate the average change in profit/loss
+        if months_total > 1:
+            change = int(profit_loss) - int(final_profit_loss)
+
+            profit_loss_sum = profit_loss_sum + change
+            profit_loss_average = profit_loss_sum / (months_total -1)
+
+#SIMON, talk about how to fix Analysis folder to make each print value print into a new line each time **SIMON
     print("Financial Analysis")
     print("------------------")
     print(months)
