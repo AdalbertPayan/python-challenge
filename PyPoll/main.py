f356adcfe3bf16146408474ac10857fe5ef5c715
Adalbert Payan <adalbertpayan@APiMac.local>
2023-07-05 18:28:36 -0700

    first update, this one is similar

---
+++ PyPoll/main.py
@@ -0,0 +1,45 @@
+import os
+import csv
+
+pypoll_csv = os.path.join('Resources', 'election_data.csv')
+
+with open(pypoll_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csv_reader)
+
+#In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
+
+#You will be given a set of poll data called election_data.csv. 
+#The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
+#Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+
+#set variable and dictionary values
+    total_votes = 0
+    candidates = {}
+    most_votes = 0
+    winner = ""
+
+#loop through the data
+    for row in csv_reader:
+        total_votes += 1
+#assign candidate column to a variable
+        candidate = row[2]
+#total number of votes cast
+        if candidate in candidates:
+            candidates[candidate] += 1
+        else: 
+            candidates[candidate] = 1
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+
+#A complete list of candidates who received votes
+
+#The percentage of votes each candidate won
+
+#total number of votes each candidate won
+
+#The winner of the election based on popular vote
